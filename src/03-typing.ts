1ac260304ba71fe37c233170914d12fc73c881cb
Armandocres <armandocres99@gmail.com>
2022-09-26 20:52:43 -0500

    types

---
+++ src/03-typing.ts
@@ -0,0 +1,15 @@
+
+(() => {
+  let myProductName = 'Producto 1';
+  let myProductPrice = 12;
+
+  // sugerir de acuerdo al tipo de dato
+  myProductName.toLowerCase();
+  myProductPrice.toFixed();
+
+  // uso de constantes
+  const myProductStock = 33;
+
+  // nos ayuda a que las constantes sigan con su valor
+  //myProductStock = 200;
+})();
---
+++ src/types/number.ts
@@ -0,0 +1,20 @@
+(() => {
+  let productPrice = 2;
+  productPrice = 4;
+
+  // forma explicita
+  let customerAge: number = 3;
+  customerAge = customerAge + 1;
+
+  // sin inicializar
+  let productInStock: number;
+
+  // sentencias con alertas
+  // if (productInStock > 10) {
+  //   console.log('is greater');
+  // }
+
+  let discount = parseInt('123');
+  console.log('disc', discount);
+
+})();
